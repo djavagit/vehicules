ac407a19ba0858271522836630acfbea5d92f1a0
djavagit <58994972+djavagit@users.noreply.github.com>
2022-01-21 11:16:02 +0100

    procfile

--- vehiculesService.py
+++ vehiculesService.py
@@ -33,5 +33,5 @@ out_protocol=Soap11())
 wsgi_application = WsgiApplication(application)
 
 if __name__ == '__main__':
-    server = make_server('0.0.0.0', 3535, wsgi_application)
+    server = make_server('0.0.0.0', 17995, wsgi_application)
     server.serve_forever()
